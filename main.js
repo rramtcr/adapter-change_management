59a7ce5be5a24059cc74d9d3818592281d12e2a3
rramtcr <rramtcr@gmail.com>
2020-05-13 13:17:09 +0000

    Final Project

--- main.js
+++ main.js
@@ -1,208 +1,134 @@
-const path = require('path');
 
-/**
-* Import the ServiceNowConnector class from local Node.js module connector.js
-*   and assign it to constant ServiceNowConnector.
-* When importing local modules, IAP requires an absolute file reference.
-* Built-in module path's join method constructs the absolute filename.
-*/
-const ServiceNowConnector = require(path.join(__dirname, '/connector.js'));
+// Import built-in Node.js package path. 
 
-/**
-* Import built-in Node.js package events' EventEmitter class and
-* assign it to constant EventEmitter. We will create a child class
-* from this class.
-*/
-const EventEmitter = require('events').EventEmitter;
 
-/**
-* The ServiceNowAdapter class.
-*
-* @summary ServiceNow Change Request Adapter
-* @description This class contains IAP adapter properties and methods that IAP
-*   brokers and products can execute. This class inherits the EventEmitter
-*   class.
-*/
-class ServiceNowAdapter extends EventEmitter {
+ 
 
-  /**
-   * Here we document the ServiceNowAdapter class' callback. It must follow IAP's
-   *   data-first convention.
-   * @callback ServiceNowAdapter~requestCallback
-   * @param {(object|string)} responseData - The entire REST API response.
-   * @param {error} [errorMessage] - An error thrown by REST API call.
-   */
+const path = require('path'); 
 
-  /**
-   * Here we document the adapter properties.
-   * @typedef {object} ServiceNowAdapter~adapterProperties - Adapter
-   *   instance's properties object.
-   * @property {string} url - ServiceNow instance URL.
-   * @property {object} auth - ServiceNow instance credentials.
-   * @property {string} auth.username - Login username.
-   * @property {string} auth.password - Login password.
-   * @property {string} serviceNowTable - The change request table name.
-   */
+const ServiceNowConnector = require(path.join(__dirname, '/connector.js')); 
 
-  /**
-   * @memberof ServiceNowAdapter
-   * @constructs
-   *
-   * @description Instantiates a new instance of the Itential ServiceNow Adapter.
-   * @param {string} id - Adapter instance's ID.
-   * @param {ServiceNowAdapter~adapterProperties} adapterProperties - Adapter instance's properties object.
-   */
-  constructor(id, adapterProperties) {
-    // Call super or parent class' constructor.
-    super();
-    // Copy arguments' values to object properties.
-    this.id = id;
-    this.props = adapterProperties;
-    // Instantiate an object from the connector.js module and assign it to an object property.
-    this.connector = new ServiceNowConnector({
-      url: this.props.url,
-      username: this.props.auth.username,
-      password: this.props.auth.password,
-      serviceNowTable: this.props.serviceNowTable
-    });
-  }
+const EventEmitter = require('events').EventEmitter; 
 
-  /**
-   * @memberof ServiceNowAdapter
-   * @method connect
-   * @summary Connect to ServiceNow
-   * @description Complete a single healthcheck and emit ONLINE or OFFLINE.
-   *   IAP calls this method after instantiating an object from the class.
-   *   There is no need for parameters because all connection details
-   *   were passed to the object's constructor and assigned to object property this.props.
-   */
-  connect() {
-    // As a best practice, Itential recommends isolating the health check action
-    // in its own method.
-    this.healthcheck();
-  }
+class ServiceNowAdapter extends EventEmitter { 
+constructor(id, adapterProperties) { 
 
-  /**
-* @memberof ServiceNowAdapter
-* @method healthcheck
-* @summary Check ServiceNow Health
-* @description Verifies external system is available and healthy.
-*   Calls method emitOnline if external system is available.
-*
-* @param {ServiceNowAdapter~requestCallback} [callback] - The optional callback
-*   that handles the response.
-*/
-healthcheck(callback) {
-this.getRecord((result, error) => {
-   /**
-    * For this lab, complete the if else conditional
-    * statements that check if an error exists
-    * or the instance was hibernating. You must write
-    * the blocks for each branch.
-    */
-   if (error) {
-     /**
-      * Write this block.
-      * If an error was returned, we need to emit OFFLINE.
-      * Log the returned error using IAP's global log object
-      * at an error severity. In the log message, record
-      * this.id so an administrator will know which ServiceNow
-      * adapter instance wrote the log message in case more
-      * than one instance is configured.
-      * If an optional IAP callback function was passed to
-      * healthcheck(), execute it passing the error seen as an argument
-      * for the callback's errorMessage parameter.
-      */
-      this.emitOffline();
-   } else {
-     /**
-      * Write this block.
-      * If no runtime problems were detected, emit ONLINE.
-      * Log an appropriate message using IAP's global log object
-      * at a debug severity.
-      * If an optional IAP callback function was passed to
-      * healthcheck(), execute it passing this function's result
-      * parameter as an argument for the callback function's
-      * responseData parameter.
-      */
-      this.emitOnline();
-   }
-});
-}
-  /**
-   * @memberof ServiceNowAdapter
-   * @method emitOffline
-   * @summary Emit OFFLINE
-   * @description Emits an OFFLINE event to IAP indicating the external
-   *   system is not available.
-   */
-  emitOffline() {
-    this.emitStatus('OFFLINE');
-    log.warn('ServiceNow: Instance is unavailable.');
-  }
+super(); 
+this.id = id; 
+this.props = adapterProperties; 
+ this.connector = new ServiceNowConnector({ 
+ url: this.props.url, 
+ username: this.props.auth.username, 
+ password: this.props.auth.password, 
+ serviceNowTable: this.props.serviceNowTable 
+}); 
 
-  /**
-   * @memberof ServiceNowAdapter
-   * @method emitOnline
-   * @summary Emit ONLINE
-   * @description Emits an ONLINE event to IAP indicating external
-   *   system is available.
-   */
-  emitOnline() {
-    this.emitStatus('ONLINE');
-    log.info('ServiceNow: Instance is available.');
-  }
+} 
 
-  /**
-   * @memberof ServiceNowAdapter
-   * @method emitStatus
-   * @summary Emit an Event
-   * @description Calls inherited emit method. IAP requires the event
-   *   and an object identifying the adapter instance.
-   *
-   * @param {string} status - The event to emit.
-   */
-  emitStatus(status) {
-    this.emit(status, { id: this.id });
-  }
+  connect() { 
 
-  /**
-   * @memberof ServiceNowAdapter
-   * @method getRecord
-   * @summary Get ServiceNow Record
-   * @description Retrieves a record from ServiceNow.
-   *
-   * @param {ServiceNowAdapter~requestCallback} callback - The callback that
-   *   handles the response.
-   */
-  getRecord(callback) {
-    /**
-     * Write the body for this function.
-     * The function is a wrapper for this.connector's get() method.
-     * Note how the object was instantiated in the constructor().
-     * get() takes a callback function.
-     */
-     this.connector.get(callback)
-  }
+    this.healthcheck(); 
+} 
 
-  /**
-   * @memberof ServiceNowAdapter
-  * @method postRecord
-   * @summary Create ServiceNow Record
-   * @description Creates a record in ServiceNow.
-   *
-   * @param {ServiceNowAdapter~requestCallback} callback - The callback that
-   *   handles the response.
-   */
-  postRecord(callback) {
-    /**
-     * Write the body for this function.
-     * The function is a wrapper for this.connector's post() method.
-     * Note how the object was instantiated in the constructor().
-     * post() takes a callback function.
-     */
-     this.connector.post(callback)
-  }
-}
+healthcheck(callback) { 
 
+this.getRecord((result, error) => { 
+
+   if (error) { 
+
+      log.error(`External system ${this.id} is temporarily down for maintenance`); 
+     this.emitOffline() 
+   }  
+
+   else { 
+log.debug("Calling ServiceNowAdaptor system's method healthcheck().") 
+console.log("I am inside else block ...................") 
+this.emitOnline() 
+ } 
+}); 
+this.postRecord((result, error) => { 
+     if (error) { 
+  log.error(`External system ${this.id} is temporarily down for maintenance`); 
+this.emitOffline() ;
+if(callback){ 
+ callback(error) 
+          } 
+}  
+ else { 
+log.debug("Calling ServiceNowAdaptor system's method healthcheck().") 
+this.emitOnline() 
+if(callback){ 
+ callback(result) 
+  } } 
+ });
+} 
+emitOffline() { 
+      this.emitStatus('OFFLINE'); 
+  log.warn('ServiceNow: Instance is unavailable.'); 
+     } 
+
+  emitOnline() { 
+      this.emitStatus('ONLINE'); 
+ log.info('ServiceNow: Instance is available.'); 
+} 
+  emitStatus(status) { 
+this.emit(status, { id: this.id }); 
+  } 
+  getRecord(callback) { 
+this.connector.get( (data, error) => { 
+ var changeTicket={} 
+ var ArrayObj = [] 
+for(var key in data) { 
+if(key === "body") { 
+ var obj= data[key]; 
+     var obj2=JSON.parse(obj); 
+    var obj3=obj2.result; 
+    console.log("the value inside post.................",obj3) 
+    for(let i=0; i<obj3.length;i++) { 
+changeTicket ={ 
+ "change_ticket_number":obj3[i].number, 
+"active":obj3[i].active, 
+  "priority":obj3[i].priority, 
+ "description":obj3[i].description, 
+"work_start":obj3[i].work_start, 
+ "work_end":obj3[i].work_end, 
+ "change_ticket_key":obj3[i].sys_id 
+} 
+ ArrayObj.push(changeTicket); 
+  } 
+}}  
+console.log("the value is:.....",ArrayObj) 
+data=ArrayObj 
+callback(data,error) 
+ }); 
+ } 
+ postRecord(callback) { 
+this.connector.post( (data, error) => { 
+var newChangeTicket={} 
+for(var key in data) 
+{ 
+ if(key === "body") 
+{ 
+ var obj= data[key]; 
+      var obj2=JSON.parse(obj); 
+ var obj3=obj2.result; 
+ console.log("the value inside post.................",obj3) 
+newChangeTicket ={ 
+ "change_ticket_number":obj3.number, 
+      "active":obj3.active, 
+"priority":obj3.priority, 
+      "description":obj3.description, 
+"work_start":obj3.work_start, 
+ "work_end":obj3.work_end, 
+"change_ticket_key":obj3.sys_id 
+} 
+}}  
+data=newChangeTicket; 
+//data["body"]["result"]= returnVar 
+callback(data,error) 
+ }); 
+} 
+  } 
 module.exports = ServiceNowAdapter;
+
+
